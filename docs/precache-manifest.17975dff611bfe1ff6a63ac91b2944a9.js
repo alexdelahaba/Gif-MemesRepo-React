8ae214aaf888945ac287e622050728d2085a7753
Alexdelahaba <alexmdelahaba@hotmail.com>
2020-07-25 09:21:35 +0200

    Carpeta docs añadida

---
+++ docs/precache-manifest.17975dff611bfe1ff6a63ac91b2944a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe26ff50a74be3a0b1eb92d266fa7695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d199ec85ec5c2f1c8942",
+    "url": "/static/css/main.33aa2db3.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d199ec85ec5c2f1c8942",
+    "url": "/static/js/main.d1208b64.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
